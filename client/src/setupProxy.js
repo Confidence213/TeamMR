f25d5a82f6c3278a0ed6e04d6ee4ca0e22117315
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-09-25 12:03:42 +0530

    updated proxy target

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3002',
+      // target: 'http://localhost:3002',
+      target: "https://api.teammanipalracing.in",
       changeOrigin: true,
       headers: {
         "Connection": "keep-alive"
